5dc3b46a7003bbb8c2ee04d07bb68968e23e617d
kimurasouichi <81295817+kimurasouichi@users.noreply.github.com>
2021-04-22 21:03:49 +0900

    ログイン機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,9 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birth_day, presence: true
   validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Include both letters and numbers' }
-  validates :last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'kana Full-width katakana characters' } 
-  validates :first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'kana Full-width katakana characters' }
   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } 
   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } 
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'kana Full-width katakana characters' } 
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'kana Full-width katakana characters' }
+  
 end
