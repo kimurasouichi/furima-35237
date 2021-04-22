4aae647b9ca8fefb90821b9da3005c279b15168e
kimurasouichi <81295817+kimurasouichi@users.noreply.github.com>
2021-04-22 11:25:12 +0900

    rspec導入とログイン機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :nickname, presence: true
-  # validates :last_name, presence: true
-  # validates :first_nama, presence: true
-  # validates :last_nama_kana, presence: true
-  # validates :first_nama_kana, presence: true
-  # validates :birth_day, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+  describe 'ユーザー新規登録' do
+    it 'emailが空では登録できないこと' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it '重複したemailが存在する場合登録できないこと' do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+    it 'emailに＠がないと登録できない' do
+      expect(@user).to be_valid
+    end
+
+    it 'email、passwordとpassword_confirmationが存在すれば登録できること' do
+      expect(@user).to be_valid
+    end
+
+    it 'passwordが6文字以上であれば登録できること' do
+      @user.password = '123456'
+      @user.password_confirmation = '123456'
+      expect(@user).to be_valid
+    end
+
+    it 'passwordとpassword_confirmationが不一致では登録できないこと' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+  end
+end
