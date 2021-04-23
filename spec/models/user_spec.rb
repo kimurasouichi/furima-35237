b046cabaef4f7f5ea940ddf8937aa2674454580b
kimurasouichi <81295817+kimurasouichi@users.noreply.github.com>
2021-04-23 20:42:10 +0900

    bundle exec rubocop -aの実施

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,9 +54,8 @@ RSpec.describe User, type: :model do
       it 'emailには「@」を含まないと登録ができない' do
         @user.email = 'tastcom'
         @user.valid?
-        expect(@user.errors.full_messages).to include "Email is invalid"
+        expect(@user.errors.full_messages).to include 'Email is invalid'
       end
-
 
       it 'passwordが空では登録できない' do
         @user.password = ''
