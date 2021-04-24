c3b01fe918882bf1a23bb565983bb4b80206e5ca
kimurasouichi <81295817+kimurasouichi@users.noreply.github.com>
2021-04-25 06:56:03 +0900

    販売価格の計算

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+function post (){
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    const addTaxPay = document.getElementById("profit");
+    addTaxPay.innerHTML = Math.floor(inputValue * 0.9 );
+  });
+};
+
+window.addEventListener('load', post);
+
