07ab225d8fff057c520943d9867cc430855bb516
kimurasouichi <81295817+kimurasouichi@users.noreply.github.com>
2021-04-25 21:26:10 +0900

    レビューの修正７

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_221402) do
+ActiveRecord::Schema.define(version: 2021_04_23_201607) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,18 +46,6 @@ ActiveRecord::Schema.define(version: 2021_04_23_221402) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "text", null: false
-    t.integer "category_id", null: false
-    t.integer "condition_id", null: false
-    t.integer "burden_id", null: false
-    t.integer "area_id", null: false
-    t.integer "day_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
