dd8fa3b78d03e178c55d6953ba80526de5f842d0
kimurasouichi <81295817+kimurasouichi@users.noreply.github.com>
2021-04-30 20:19:09 +0900

    購入機能保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,30 +1,34 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
-    @purchase_order = Purchase.new
+    @purchase_order = PurchaseOrder.new
   end
 
   def create
-    @purchase_order = Purchase.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    @purchase_order = PurchaseOrder.new(purchase_params)
     if @purchase_order.valid?
       pay_item
       @purchase_order.save
       redirect_to root_path
     else
-      render shared / error_messages
+      render :index
     end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase_order).permit(:user_id, :item_id).marge(user_id: current_user.id, item_id: current_user.id,
-                                                                     token: params[:token])
+    params.require(:purchase_order).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: current_user.id,
+                                                                                                                       token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']    # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
       card: purchase_params[:token], # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
